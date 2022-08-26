d000a2a54ba6e17de5c07e1a05aa4f7e2e7aebde
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-08-26 18:16:57 -0300

    quarto requisito feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,21 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const managers = data.employees.find((manager) => manager.id === id).managers.length < 2;
+  return managers;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const employees = data.employees.filter((employee) => employee.managers.includes(managerId));
+
+  const arrayWithNames = employees.map((employee) => `${employee.firstName} ${employee.lastName}`);
+
+  return arrayWithNames;
 }
+getRelatedEmployees();
 
 module.exports = { isManager, getRelatedEmployees };
