e0bdb14fda03d0b1f8f3ff9f9cfa00a1db0ca682
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-08-28 14:23:59 -0300

    quinto requisito feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const sum = species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
+  if (animal === undefined) {
+    return sum;
+  }
+  if (animal.specie && animal.sex) {
+    const ani = species.find((name) => species.name === name.specie).residents;
+    const sex = ani.filter((sexo) => sexo.sex === animal.sex).length;
+    return sex;
+  }
+  if (animal.specie) {
+    const quant = species.find((count) => species.name === count.specie).residents.length;
+    return quant;
+  }
 }
-
+countAnimals();
 module.exports = countAnimals;
