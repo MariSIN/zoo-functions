34a5e9aa9841baa4c5c1519699b7582964b60879
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-08-29 17:56:09 -0300

    corrige lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,33 +1,10 @@
 const data = require('../data/zoo_data');
 
-const entrants = [
-	{ name:  'Lara Carvalho', age:  5 },
-	{ name:  'Frederico Moreira', age:  5 },
-	{ name:  'Pedro Henrique Carvalho', age:  5 },
-	{ name:  'Maria Costa', age:  18 },
-	{ name:  'Núbia Souza', age:  18 },
-	{ name:  'Carlos Nogueira', age:  50 },
-];
-
 function countEntrants(entrants) {
-  const child = entrants.filter((ages) => ages.age < 18).length;
-  const adult = entrants.filter((ages) => ages.age >= 18 && ages.age < 50).length;
-  const senior = entrants.filter((ages) => ages.age >= 50).length;
-  return {child: child, adult: adult, senior: senior};
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
-  if (entrants === undefined || entrants === {}) {
-    return 0;
-  }
-  if (entrants.adult) {
-    const getEntrantsAdutls = entrants.filter((ages) => ages.age >= 18 && ages.age < 50).length;
-    const price = data.prices.filter((price) => price.adult)
-    console.log(price)
-  } 
 }
-console.log(calculateEntry(entrants));
-console.log(countEntrants(entrants));
 
 module.exports = { calculateEntry, countEntrants };
