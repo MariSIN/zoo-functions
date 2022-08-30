52d8d76a3eee33ce0f832f6c72d72b9ceeda1c7c
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-08-30 17:46:34 -0300

    requisito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((e) => e.id === id);
+  let age = 0;
+  const animals = data.species.find((animal) => animal.id === employee.responsibleFor[0]).residents;
+  for (let i = 0; i < animals.length; i += 1) {
+    const animal = animals[i];
+    if (animal.age > age) {
+      age = animal.age;
+    }
+  }
+  const result = animals.find((a) => a.age === age);
+  const arr = [];
+  arr.push(result.name);
+  arr.push(result.sex);
+  arr.push(result.age);
+  console.log(result);
+  return arr;
 }
-
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 module.exports = getOldestFromFirstSpecies;
