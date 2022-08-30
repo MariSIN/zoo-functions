7315a46382355f69f24ee46cfc1cffff79d09b36
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-08-30 16:57:07 -0300

    requisito 9 feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,31 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const informations = () => employees.map((a) => ({
+  id: a.id,
+  fullName: `${a.firstName} ${a.lastName}`,
+  species: a.responsibleFor.map((r) => species.find((s) => s.id === r).name),
+  locations: a.responsibleFor.map((r) => species.find((s) => s.id === r).location),
+}));
 
+function getEmployeesCoverage(object) {
+  if (object === undefined) {
+    return informations();
+  }
+  const verify = employees.some((employ) =>
+    employ.firstName === object.name
+|| employ.lastName === object.name || employ.id === object.id);
+  if (!verify) {
+    throw new Error('Informações inválidas');
+  }
+  const emp = employees.find((employee) => employee.firstName === object.name
+  || employee.lastName === object.name || employee.id === object.id);
+  const obj = {
+    id: emp.id,
+    fullName: `${emp.firstName} ${emp.lastName}`,
+    species: emp.responsibleFor.map((r) => species.find((s) => s.id === r).name),
+    locations: emp.responsibleFor.map((r) => species.find((s) => s.id === r).location),
+  };
+  return obj;
+}
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+/*
+const expected = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+  Friday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
+  },
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: [
+      'lions', 'tigers',
+      'bears', 'penguins',
+      'otters', 'frogs',
+      'snakes', 'elephants',
+    ],
+  },
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+*/
 
+function getSchedule(scheduleTarget) {
+  if (species.some((animal) => animal.name === scheduleTarget)) {
+    const daysForVisity = species.find((nome) => nome.name === scheduleTarget).availability;
+    return daysForVisity;
+  }
+}
+getSchedule();
 module.exports = getSchedule;
